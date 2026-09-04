361d73901865f462182e0d9266eff35f5b4f4e58
Umesh Sharma <sharmaanjumesh@gmail.com>
2026-09-04 10:27:23 +0530

    Time: 1 ms (99.79%), Space: 46.5 MB (32.13%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n=nums.length;
+            int[] suffix=new int[n];
+            suffix[n-1]=nums[n-1];
+            for(int i=n-2;i>=0;i--){
+                suffix[i]=Math.min(nums[i],suffix[i+1]);
+            }
+
+            int prefixmax=nums[0];
+            for(int i=0;i<n;i++){
+                prefixmax=Math.max(prefixmax,nums[i]);
+                if(prefixmax-suffix[i]<=k){
+                    return i;
+                }
+            }
+        return -1;
+    }
+}
